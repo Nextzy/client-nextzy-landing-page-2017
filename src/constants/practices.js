deacfe05dfccc9bc27a3f2da09634272c5be744f
PNNutkung <pipatpol.t@ku.th>
2017-08-29 17:05:32 +0700

    Add practice constants

---
+++ src/constants/practices.js
@@ -0,0 +1,14 @@
+export const practices = [
+  {
+    icon: 'icon-focus',
+    text: 'Pixel Perfect'
+  },
+  {
+    icon: 'icon-layers',
+    text: 'Cutting-Edge Technologies'
+  },
+  {
+    icon: 'icon-linegraph',
+    text: 'Lightning Speed Development'
+  }
+]
