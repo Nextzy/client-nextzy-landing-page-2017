8150dd7f6ce42714eead3ba7eb515738f333bcf1
PNNutkung <pipatpol.t@ku.th>
2017-08-30 11:48:00 +0700

    Add all menu items

---
+++ src/constants/menus.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    key: 1,
+    href: '#home',
+    name: 'Home'
+  },
+  {
+    key: 2,
+    href: '#services',
+    name: 'Services'
+  },
+  {
+    key: 3,
+    href: '#works',
+    name: 'Our works'
+  },
+  {
+    key: 4,
+    href: '#team',
+    name: 'Team'
+  },
+  {
+    key: 5,
+    href: '#contact',
+    name: 'Contact'
+  },
+  {
+    key: 6,
+    href: 'https://blog.nextzy.me/',
+    name: 'Blog'
+  },
+  {
+    key: 7,
+    href: '/careers',
+    name: 'Careers'
+  }
+]
