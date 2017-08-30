d63347455862827cbaa33ca2bc4c9a5c28ebbf69
PNNutkung <pipatpol.t@ku.th>
2017-08-30 11:47:40 +0700

    Add Navigation bar

---
+++ src/containers/navbar/HeaderNavigation.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import menus from '../../constants/menus'
+
+class HeaderNavigation extends React.Component {
+  constructor(props) {
+    super(props)
+    this._renderMenu = this._renderMenu.bind(this)
+  }
+
+  _renderMenu(data) {
+    if (data !== null) {
+      return data.map(menu => {
+        return (
+          <NavItem key={menu.key} eventKey={menu.key} href={menu.href}>
+            {menu.name}
+          </NavItem>
+        )
+      })
+    }
+  }
+
+  render() {
+    return (
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">Nextzy</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>{this._renderMenu(menus)}</Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
+}
+
+export default HeaderNavigation
