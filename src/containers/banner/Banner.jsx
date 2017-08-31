39e820b2396a1824fd10ec908e0d80984a8ce243
PNNutkung <pipatpol.t@ku.th>
2017-08-31 16:38:36 +0700

    Change background to image source

--- src/containers/banner/Banner.jsx
+++ src/containers/banner/Banner.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
-import { Col } from 'react-bootstrap'
 import banner from '../../images/banner_header.jpg'
 import './Banner.css'
-
-const appStyle = {
-  backgroundImage: `url(${banner})`
-}
 
 class Banner extends React.Component {
   render() {
     return (
       <section className="Banner">
         <div className="top-banner">
-          <Col xs={12} sm={12} md={6} lg={6} />
-          <Col className="description" xs={12} sm={12} md={6} lg={6}>
+          <img className="background-image" src={banner} alt="Nextzy" />
+          <div className="description">
             <h1 className="text-title">{this.props.nextzy}</h1>
             <h1 className="text-title">{this.props.tech}</h1>
             <h6 className="text-subtitle">{this.props.subtitle}</h6>
-          </Col>
+          </div>
         </div>
       </section>
     )
