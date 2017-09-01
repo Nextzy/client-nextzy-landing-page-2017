840cc5d15579ed527cfd42d254d9ff863157911e
PNNutkung <pipatpol.t@ku.th>
2017-09-01 12:04:14 +0700

    Add css files

---
+++ src/components/WorkTitle.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './WorkTitle.css'
+
+class WorkTitle extends React.Component {
+  render() {
+    return (
+      <div className="WorkTitle">
+        <h1>Our Works</h1>
+      </div>
+    )
+  }
+}
+
+export default WorkTitle
