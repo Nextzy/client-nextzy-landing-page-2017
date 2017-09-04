a4079273f5127755b10265d8ea57a4983bdd6c36
PNNutkung <pipatpol.t@ku.th>
2017-09-04 12:10:36 +0700

    Can show clients correctly

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -5,8 +5,10 @@ class Client extends React.Component {
   render() {
     return (
       <div className="Client">
-        <div className="wrapper">
-          <img src={this.props.img} alt={this.props.name} />
+        <div className="outer-frame">
+          <div className="wrapper">
+            <img src={this.props.img} alt={this.props.name} />
+          </div>
         </div>
       </div>
     )
--- src/containers/clients/Clients.jsx
+++ src/containers/clients/Clients.jsx
@@ -4,15 +4,47 @@ import Slider from 'react-slick'
 import Client from '../../components/Client'
 import { clients } from '../../constants/clients'
 import './Clients.css'
+import generator from 'generate-password'
 
 const settings = {
   autoplay: true,
   infinite: true,
   dots: true,
   speed: 500,
-  draggable: true,
-  fade: true,
-  vertical: false
+  pauseOnHover: true,
+  responsive: [
+    {
+      breakpoint: 100000,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 }
 
 class Clients extends React.Component {
@@ -24,7 +56,11 @@ class Clients extends React.Component {
   _renderClient(data) {
     if (data !== null) {
       return data.map(client => (
-        <Client key={client.name} img={client.img} name={client.name} />
+        <Client
+          key={generator.generate({ length: 18, numbers: true, symbols: true })}
+          img={client.img}
+          name={client.name}
+        />
       ))
     }
   }
@@ -32,7 +68,19 @@ class Clients extends React.Component {
   render() {
     return (
       <Col className="Clients" xs={12} sm={12} md={12} lg={12}>
-        <Slider {...settings}>{this._renderClient(clients)}</Slider>
+        <Slider className="clients-slider" {...settings}>
+          {this._renderClient(clients).map(slide => (
+            <div
+              key={generator.generate({
+                length: 18,
+                numbers: true,
+                symbols: true
+              })}
+            >
+              {slide}
+            </div>
+          ))}
+        </Slider>
       </Col>
     )
   }
