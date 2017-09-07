0bd962b1b69a305761cccad4d26d099e19376839
Akexorcist <akexorcist@gmail.com>
2017-09-07 16:39:15 +0700

    Add more members

--- src/constants/members.js
+++ src/constants/members.js
@@ -1,5 +1,5 @@
-import normal from '../images/person_sample_guy_normal.png'
-import hover from '../images/person_sample_guy_hover.png'
+import coming_soon_normal from '../images/person_coming_soon_normal.png'
+import coming_soon_hover from '../images/person_coming_soon_hover.png'
 
 const members = [
   {
@@ -7,10 +7,11 @@ const members = [
     last_name: 'Nakpong',
     position: 'Founder and CEO',
     image: {
-      normal: normal,
-      hover: hover
+      normal: require('../images/person_palakorn_nakphong_normal.png'),
+      hover: require('../images/person_palakorn_nakphong_hover.png')
     },
-    facebook: 'a',
+
+    facebook: 'codingz',
     github: 'a',
     linkedin: 'a'
   },
@@ -19,20 +20,176 @@ const members = [
     last_name: 'Jampa',
     position: 'Founder and CTO',
     image: {
-      normal: normal,
-      hover: hover
+      normal: require('../images/person_tanpisit_jampa_normal.png'),
+      hover: require('../images/person_tanpisit_jampa_hover.png')
     },
     facebook: 'b',
     github: 'b',
     linkedin: 'b'
   },
   {
+    first_name: 'Atthawit',
+    last_name: 'Muansiri',
+    position: 'Web Developer',
+    image: {
+      normal: require('../images/person_atthawit_muansiri_normal.png'),
+      hover: require('../images/person_atthawit_muansiri_hover.png')
+    },
+    facebook: 'c',
+    github: 'c',
+    linkedin: 'c'
+  },
+  {
+    first_name: 'Awika',
+    last_name: 'Kadeetong',
+    position: 'System Analyst',
+    image: {
+      normal: require('../images/person_awika_kadeetong_normal.png'),
+      hover: require('../images/person_awika_kadeetong_hover.png')
+    },
+    facebook: 'c',
+    github: 'c',
+    linkedin: 'c'
+  },
+  {
+    first_name: 'Chanchai',
+    last_name: 'Tasujai',
+    position: 'Automated Tester',
+    image: {
+      normal: require('../images/person_chanchai_tasujai_normal.png'),
+      hover: require('../images/person_chanchai_tasujai_hover.png')
+    },
+    facebook: 'c',
+    github: 'c',
+    linkedin: 'c'
+  },
+  {
+    first_name: 'Charuwit',
+    last_name: 'Nodthaisong',
+    position: 'Web Developer',
+    image: {
+      normal: require('../images/person_charuwit_nodthaisong_normal.png'),
+      hover: require('../images/person_charuwit_nodthaisong_hover.png')
+    },
+    facebook: 'c',
+    github: 'c',
+    linkedin: 'c'
+  },
+  {
+    first_name: 'Hatsanai',
+    last_name: 'Decha',
+    position: 'Web Developer',
+    image: {
+      normal: coming_soon_normal,
+      hover: coming_soon_hover
+    },
+    facebook: 'c',
+    github: 'c',
+    linkedin: 'c'
+  },
+  {
+    first_name: 'Kanjana',
+    last_name: 'Khamkaew',
+    position: 'Software Tester',
+    image: {
+      normal: coming_soon_normal,
+      hover: coming_soon_hover
+    },
+    facebook: 'c',
+    github: 'c',
+    linkedin: 'c'
+  },
+  {
+    first_name: 'ManhTien',
+    last_name: 'Lo',
+    position: 'iOS Developer',
+    image: {
+      normal: require('../images/person_manhtien_lo_normal.png'),
+      hover: require('../images/person_manhtien_lo_hover.png')
+    },
+    facebook: 'c',
+    github: 'c',
+    linkedin: 'c'
+  },
+  {
+    first_name: 'Mullika',
+    last_name: 'Roungkome',
+    position: 'Web Developer',
+    image: {
+      normal: coming_soon_normal,
+      hover: coming_soon_hover
+    },
+    facebook: 'c',
+    github: 'c',
+    linkedin: 'c'
+  },
+  {
+    first_name: 'Navaporn',
+    last_name: 'Rukthongthai',
+    position: 'System Analyst',
+    image: {
+      normal: require('../images/person_navaporn_rukthongthai_normal.png'),
+      hover: require('../images/person_navaporn_rukthongthai_hover.png')
+    },
+    facebook: 'c',
+    github: 'c',
+    linkedin: 'c'
+  },
+  {
+    first_name: 'Nonthawit',
+    last_name: 'Doungsodsri',
+    position: 'Android Developer',
+    image: {
+      normal: require('../images/person_nonthawit_doungsodsri_normal.png'),
+      hover: require('../images/person_nonthawit_doungsodsri_hover.png')
+    },
+    facebook: 'c',
+    github: 'c',
+    linkedin: 'c'
+  },
+  {
+    first_name: 'Panaya',
+    last_name: 'Loymee',
+    position: 'Automated Test',
+    image: {
+      normal: coming_soon_normal,
+      hover: coming_soon_hover
+    },
+    facebook: 'e',
+    github: 'e',
+    linkedin: 'e'
+  },
+  {
+    first_name: 'Panupak',
+    last_name: 'Vichaidit',
+    position: 'Web Developer',
+    image: {
+      normal: require('../images/person_panupak_vichaidit_normal.png'),
+      hover: require('../images/person_panupak_vichaidit_hover.png')
+    },
+    facebook: 'e',
+    github: 'e',
+    linkedin: 'e'
+  },
+  {
     first_name: 'Phutsadee',
     last_name: 'Donyangphai',
-    position: 'Project coordinator',
+    position: 'Project Coordinator',
     image: {
-      normal: normal,
-      hover: hover
+      normal: require('../images/person_phutsadee_donyangphai_normal.png'),
+      hover: require('../images/person_phutsadee_donyangphai_hover.png')
+    },
+    facebook: 'c',
+    github: 'c',
+    linkedin: 'c'
+  },
+  {
+    first_name: 'Royle',
+    last_name: 'Koonlert',
+    position: 'Android Developer',
+    image: {
+      normal: coming_soon_normal,
+      hover: coming_soon_hover
     },
     facebook: 'c',
     github: 'c',
@@ -40,39 +197,123 @@ const members = [
   },
   {
     first_name: 'Somkiat',
-    last_name: 'Kitwongwattana',
+    last_name: 'Khitwongwattana',
     position: 'Android Developer',
     image: {
-      normal: normal,
-      hover: hover
+      normal: require('../images/person_somkiat_khitwongwattana_normal.png'),
+      hover: require('../images/person_somkiat_khitwongwattana_hover.png')
     },
     facebook: 'd',
     github: 'd',
     linkedin: 'd'
   },
   {
-    first_name: 'Panupak',
-    last_name: 'Vichaidit',
-    position: 'Java Developer',
+    first_name: 'Supakorn',
+    last_name: 'Thongtra',
+    position: 'Web Developer',
     image: {
-      normal: normal,
-      hover: hover
+      normal: require('../images/person_supakorn_thongtra_normal.png'),
+      hover: require('../images/person_supakorn_thongtra_hover.png')
     },
-    facebook: 'e',
-    github: 'e',
-    linkedin: 'e'
+    facebook: 'd',
+    github: 'd',
+    linkedin: 'd'
+  },
+  {
+    first_name: 'Supamas',
+    last_name: 'Vatcharakajon',
+    position: 'System Analyst',
+    image: {
+      normal: require('../images/person_supamas_vatcharakajon_normal.png'),
+      hover: require('../images/person_supamas_vatcharakajon_hover.png')
+    },
+    facebook: 'd',
+    github: 'd',
+    linkedin: 'd'
+  },
+  {
+    first_name: 'Sutinee',
+    last_name: 'Doungvisut',
+    position: 'Android Developer',
+    image: {
+      normal: require('../images/person_sutinee_doungvisut_normal.png'),
+      hover: require('../images/person_sutinee_doungvisut_hover.png')
+    },
+    facebook: 'd',
+    github: 'd',
+    linkedin: 'd'
+  },
+  {
+    first_name: 'Taksaporn',
+    last_name: 'Auttamo',
+    position: 'Software Tester',
+    image: {
+      normal: coming_soon_normal,
+      hover: coming_soon_hover
+    },
+    facebook: 'f',
+    github: 'f',
+    linkedin: 'f'
+  },
+  {
+    first_name: 'Tanalat',
+    last_name: 'Boonfuang',
+    position: 'iOS Developer',
+    image: {
+      normal: coming_soon_normal,
+      hover: coming_soon_hover
+    },
+    facebook: 'f',
+    github: 'f',
+    linkedin: 'f'
   },
   {
     first_name: 'Tanapruk',
     last_name: 'Tangphianphan',
     position: 'Android Developer',
     image: {
-      normal: normal,
-      hover: hover
+      normal: require('../images/person_tanapruk_tangphianphan_normal.png'),
+      hover: require('../images/person_tanapruk_tangphianphan_hover.png')
     },
     facebook: 'f',
     github: 'f',
     linkedin: 'f'
+  },
+  {
+    first_name: 'Tararat',
+    last_name: 'Wongsansee',
+    position: 'Automated Tester',
+    image: {
+      normal: coming_soon_normal,
+      hover: coming_soon_hover
+    },
+    facebook: 'f',
+    github: 'f',
+    linkedin: 'f'
+  },
+  {
+    first_name: 'Thongpak',
+    last_name: 'Pongsilathong',
+    position: 'iOS Developer',
+    image: {
+      normal: require('../images/person_thongpak_pongsilathong_normal.png'),
+      hover: require('../images/person_thongpak_pongsilathong_hover.png')
+    },
+    facebook: 'd',
+    github: 'd',
+    linkedin: 'd'
+  },
+  {
+    first_name: 'Worakarn',
+    last_name: 'Pulsuk',
+    position: 'System Analyst',
+    image: {
+      normal: require('../images/person_worakarn_pulsuk_normal.png'),
+      hover: require('../images/person_worakarn_pulsuk_hover.png')
+    },
+    facebook: 'd',
+    github: 'd',
+    linkedin: 'd'
   }
 ]
 
