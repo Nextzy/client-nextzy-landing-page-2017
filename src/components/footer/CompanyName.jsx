6c264a840336e2d4a046814def9365a18783d53a
Akexorcist <akexorcist@gmail.com>
2017-09-07 17:12:50 +0700

    Update copyright from 2016 to 2017

--- src/components/footer/CompanyName.jsx
+++ src/components/footer/CompanyName.jsx
@@ -6,7 +6,7 @@ class CompanyName extends React.Component {
   render() {
     return (
       <Col xs={12} sm={12} md={6} lg={6} className="company-name">
-        <h4>&copy; 2016 Nextzy Technologies Co., Ltd.</h4>
+        <h4>&copy; 2017 Nextzy Technologies Co., Ltd.</h4>
       </Col>
     )
   }
