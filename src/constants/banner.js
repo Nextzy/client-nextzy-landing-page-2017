1577298a2eb1e54f2f98bc6296d941234f02e31b
PNNutkung <pipatpol.t@ku.th>
2017-09-07 12:02:19 +0700

    Change our practices css

--- src/constants/banner.js
+++ src/constants/banner.js
@@ -1,5 +1,5 @@
-const nextzy = 'Nextzy'
-const tech = 'Technologies'
+const nextzy = 'N E X T Z Y'
+const tech = 'T E C H N O L G I E S'
 const subtitle =
   'We are pirates. We sail and hunt the best mobile and web solution.'
 
