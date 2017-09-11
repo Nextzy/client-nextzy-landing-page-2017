f9f89490bd7141a5123ce85783cab93b65709b88
PNNutkung <pipatpol.t@ku.th>
2017-09-11 12:21:56 +0700

    Chagne footer wrap

--- src/components/footer/CompanyName.jsx
+++ src/components/footer/CompanyName.jsx
@@ -5,7 +5,7 @@ import './CompanyName.css'
 class CompanyName extends React.Component {
   render() {
     return (
-      <Col xs={12} sm={12} md={6} lg={6} className="company-name">
+      <Col xs={12} sm={12} md={6} lg={6} className="CompanyName">
         <h4>&copy; 2017 Nextzy Technologies Co., Ltd.</h4>
       </Col>
     )
