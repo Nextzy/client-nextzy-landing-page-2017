843cec7b65eea603807c55c483a04b9875f19199
PNNutkung <pipatpol.t@ku.th>
2017-09-12 14:25:01 +0700

    Add favicons

--- src/containers/app/App.test.js
+++ src/containers/app/App.test.js
@@ -1,8 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4)
+  })
 })
