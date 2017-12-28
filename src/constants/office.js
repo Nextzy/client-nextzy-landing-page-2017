a5d7ddfe69bfcf4e56e72abaf605e563f30b3bfb
Akexorcist <akexorcist@gmail.com>
2017-12-28 15:06:19 +0700

    Fix nextzy cnx location

--- src/constants/office.js
+++ src/constants/office.js
@@ -7,8 +7,8 @@ const office = {
       'Nextzy Technologies Co., Ltd. (Bangkok)\n219 / 22 Asoke Towers 7th\nSoi Sukhumvit 21 Rd., Klongtoey Nua,\nWattana, Bangkok 10110, Thailand.'
   },
   chiangmai: {
-    lat: 18.795208,
-    lng: 98.972442,
+    lat: 18.795211,
+    lng: 98.9724383,
     key: 'Cube No.7',
     address:
       'Nextzy Technologies Co., Ltd. (Chiang Mai)\n17 / 1 Cube No.7 Sirimangkalajarn Lane 7\nSuthep, Muang, 50200, Thailand.'
