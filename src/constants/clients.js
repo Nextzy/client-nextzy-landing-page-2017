7ff588667c4a3d187c910cb72d005281fdb17e76
Akexorcist <akexorcist@gmail.com>
2017-12-28 11:29:48 +0700

    Add more our clients

--- src/constants/clients.js
+++ src/constants/clients.js
@@ -1,6 +1,8 @@
 import ais from '../images/logos/logo_ais.png'
 import nitto from '../images/logos/nextzy_landing_page_client_nitto.jpg'
 import trueCompany from '../images/logos/nextzy_landing_page_client_true.jpg'
+import tip from '../images/logos/nextzy_landing_page_client_tip.jpg'
+import iteam from '../images/logos/nextzy_landing_page_client_iteam.jpg'
 
 const clients = [
   {
@@ -14,6 +16,14 @@ const clients = [
   {
     img: trueCompany,
     name: 'True'
+  },
+  {
+    img: tip,
+    name: 'Dhipaya Insurance'
+  },
+  {
+    img: iteam,
+    name: 'iTeam Studio'
   }
 ]
 
