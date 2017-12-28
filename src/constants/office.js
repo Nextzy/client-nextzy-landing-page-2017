44ce17182cd379ac96e782194067f2798011631d
Akexorcist <akexorcist@gmail.com>
2017-12-28 15:02:12 +0700

    Add nextzy cnx address info

--- src/constants/office.js
+++ src/constants/office.js
@@ -4,13 +4,14 @@ const office = {
     lng: 100.5623,
     key: 'Asoke Tower',
     address:
-      'Nextzy Technologies Co., Ltd.219 / 22 Asoke Towers 7th\nSoi Sukhumvit 21 Rd., Klongtoey Nua,\nWattana, Bangkok 10110, Thailand.'
+      'Nextzy Technologies Co., Ltd. (Bangkok)\n219 / 22 Asoke Towers 7th\nSoi Sukhumvit 21 Rd., Klongtoey Nua,\nWattana, Bangkok 10110, Thailand.'
   },
   chiangmai: {
     lat: 18.795208,
     lng: 98.972442,
     key: 'Chiang Mai',
-    address: 'Nextzy CNX'
+    address:
+      'Nextzy Technologies Co., Ltd. (Chiang Mai)\n17 / 1 Cube No.7 Sirimangkalajarn Lane 7\nSuthep, Muang, 50200, Thailand.'
   }
 }
 
