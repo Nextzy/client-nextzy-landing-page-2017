e17df7fe783a49183335c19dab6388acce4fde08
Akexorcist <akexorcist@gmail.com>
2017-12-28 15:12:48 +0700

    Remove unneccessary address name

--- src/constants/office.js
+++ src/constants/office.js
@@ -4,14 +4,14 @@ const office = {
     lng: 100.5623,
     key: 'Asoke Tower',
     address:
-      'Nextzy Technologies Co., Ltd. (Bangkok)\n219 / 22 Asoke Towers 7th\nSoi Sukhumvit 21 Rd., Klongtoey Nua,\nWattana, Bangkok 10110, Thailand.'
+      '219 / 22 Asoke Towers 7th, Soi Sukhumvit 21 Rd., Klongtoey Nua,\nWattana, Bangkok 10110, Thailand.'
   },
   chiangmai: {
     lat: 18.795211,
     lng: 98.9724383,
     key: 'Cube No.7',
     address:
-      'Nextzy Technologies Co., Ltd. (Chiang Mai)\n17 / 1 Cube No.7 Sirimangkalajarn Lane 7\nSuthep, Muang, 50200, Thailand.'
+      '17 / 1 Cube No.7 Sirimangkalajarn Lane 7\nSuthep, Muang, 50200, Thailand.'
   }
 }
 
